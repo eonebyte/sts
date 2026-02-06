a19e687ea37d5daad920d460cecfc258319ba253
Eonebyte <eonebyte@gmail.com>
2026-02-06 13:26:36 +0700

    Update datatable: hilangkan props di yang menggunakannya

--- client/app/(dashboard)/delivery/fromdpk/page.tsx
+++ client/app/(dashboard)/delivery/fromdpk/page.tsx
@@ -124,7 +124,6 @@ export default function ReceiptPage() {
             <DataTable
                 columns={columns}
                 data={shipments}
-                placeholder="Cari No. Surat Jalan..."
                 rowSelection={rowSelection}
                 setRowSelection={setRowSelection}
                 loading={loading}

c05085f8e2ee8943192f0f8343915f0aed090a88
Eonebyte <eonebyte@gmail.com>
2026-02-20 13:54:48 +0700

    Fixed : From Driver

--- web_service/internal/shipment/repository.go
+++ web_service/internal/shipment/repository.go
@@ -598,8 +598,8 @@ func (r *oraRepo) GetComeback(from, to time.Time) ([]Shipment, error) {
 		LEFT JOIN ADW_TMS_TNKB att ON att.ADW_TMS_TNKB_ID = sts.TNKB_ID 
 		WHERE mi.movementdate >= :1
 		  AND mi.movementdate < :2
---		  AND mi.ADW_TMS_ID IS NULL
---  		  AND mi.C_INVOICE_ID IS NULL
+--		  -- AND mi.ADW_TMS_ID IS NULL
+--  	  -- AND mi.C_INVOICE_ID IS NULL
 		  AND mi.IsSoTrx = 'Y'
 		  AND mi.INSTS = 'Y'
 		  AND sts.STATUS IN ('HO: DPK_TO_DRIVER', 'HO: DRIVER_CHECKIN', 'HO: DRIVER_CHECKOUT')
